aff1746cdeecd51741b27152ef74aa4cc008c368
cesarabs54 <46433735+cesarabs54@users.noreply.github.com>
2025-08-30 11:57:04 -0500

    Refactor validation error handling to provide detailed messages

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/util/RequestValidator.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/util/RequestValidator.java
@@ -1,13 +1,13 @@
 package co.com.bancolombia.api.util;
 
 import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -15,11 +15,13 @@ public class RequestValidator {
 
     private final Validator validator;
 
-
     public <T> Mono<T> validate(T request) {
         Set<ConstraintViolation<T>> violations = validator.validate(request);
         if (!violations.isEmpty()) {
-            throw new ConstraintViolationException(violations);
+            String errorMessage = violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining(", "));
+            return Mono.error(new IllegalArgumentException("Errores de validación: " + errorMessage));
         }
         return Mono.just(request);
     }
